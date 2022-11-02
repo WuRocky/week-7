35b45ff04f8dc8a856dc53dc43cfadfce0c3199e
Rocky Wu <rockywu971@gmail.com>
2022-11-02 13:39:48 +0800

    2022/11/02 13:39

--- app.py
+++ app.py
@@ -170,7 +170,6 @@ def api():
         success__updata_name={
         "ok":True
         }
-
         mycursor.execute("update member set name = %s where id = %s",(updata_neame,session["id"],))
         connection.commit()
         if mycursor.rowcount != None:
